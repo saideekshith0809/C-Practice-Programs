dc0b5446c0236b981c3e58e4c87adaa1f945a302
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-05 21:42:06 +0530

    Add function to find min operations for alternating binary
    Implement function to calculate minimum operations to convert a binary string to alternating format.

---
+++ AlternatingBinaryString.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+int minOperations(char *s) {
+    int n = strlen(s);
+    int changes0 = 0, changes1 = 0;
+
+    for(int i = 0; i < n; i++) {
+        char expected0 = (i % 2 == 0) ? '0' : '1';
+        char expected1 = (i % 2 == 0) ? '1' : '0';
+
+        if(s[i] != expected0)
+            changes0++;
+
+        if(s[i] != expected1)
+            changes1++;
+    }
+
+    return changes0 < changes1 ? changes0 : changes1;
+}
