5569faa3c678381a22ea95543e81e08e9cea68e7
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-06 20:56:45 +0530

    Implement function to add two numbers represented as lists

---
+++ AddTwoNumbers.c
@@ -0,0 +1,33 @@
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    
+    struct ListNode dummy;
+    struct ListNode *temp = &dummy;
+    dummy.next = NULL;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        struct ListNode *newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+
+        temp->next = newNode;
+        temp = temp->next;
+    }
+
+    return dummy.next;
+}
