9eb67613efbfd714de2c8e09466a64c4dcd2f498
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-11 20:28:04 +0530

    Create SequentialClamp.c

---
+++ SequentialClamp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int clamp(int y, int a, int b) {
+    if (y < a) return a;
+    if (y > b) return b;
+    return y;
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+
+        int Y = 1000000000; // very large number
+
+        for (int i = 0; i < N; i++) {
+            int A, B;
+            scanf("%d %d", &A, &B);
+
+            Y = clamp(Y, A, B);
+        }
+
+        printf("%d\n", Y);
+    }
+
+    return 0;
+}
