09e3950e9d338b441146afe1ba97761aacc88a02
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-12 21:44:33 +0530

    Update TwoSum.c

--- TwoSum.c
+++ TwoSum.c
@@ -1,41 +1,64 @@
 #include <stdlib.h>
 
-#define HASH_SIZE 20001
+#define HASH_SIZE 1000
 
-int hashKey[HASH_SIZE];
-int hashVal[HASH_SIZE];
+struct Node {
+    int key;
+    int index;
+    struct Node* next;
+};
 
-int hash(int key){
-    if(key < 0) key = -key;
+struct Node* hashTable[HASH_SIZE];
+
+int hash(int key) {
+    if (key < 0) key = -key;
     return key % HASH_SIZE;
+}
+
+void insert(int key, int index) {
+    int h = hash(key);
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->key = key;
+    newNode->index = index;
+
+    newNode->next = hashTable[h];
+    hashTable[h] = newNode;
+}
+
+struct Node* search(int key) {
+    int h = hash(key);
+    struct Node* temp = hashTable[h];
+
+    while (temp != NULL) {
+        if (temp->key == key)
+            return temp;
+        temp = temp->next;
+    }
+
+    return NULL;
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
-    for(int i = 0; i < HASH_SIZE; i++)
-        hashKey[i] = -1;
+    for (int i = 0; i < HASH_SIZE; i++)
+        hashTable[i] = NULL;
 
-    for(int i = 0; i < numsSize; i++){
+    for (int i = 0; i < numsSize; i++) {
+
         int complement = target - nums[i];
-        int h = hash(complement);
 
-        while(hashKey[h] != -1){
-            if(hashKey[h] == complement){
-                int* result = malloc(2 * sizeof(int));
-                result[0] = hashVal[h];
-                result[1] = i;
-                *returnSize = 2;
-                return result;
-            }
-            h = (h + 1) % HASH_SIZE;
+        struct Node* found = search(complement);
+
+        if (found != NULL) {
+            int* result = (int*)malloc(2 * sizeof(int));
+            result[0] = found->index;
+            result[1] = i;
+            *returnSize = 2;
+            return result;
         }
 
-        h = hash(nums[i]);
-        while(hashKey[h] != -1)
-            h = (h + 1) % HASH_SIZE;
-
-        hashKey[h] = nums[i];
-        hashVal[h] = i;
+        insert(nums[i], i);
     }
 
     *returnSize = 0;
