41a2ff147ddddfb096a82bd793eb5256c1770395
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-12 21:43:41 +0530

    Update Color Festival.c

--- Color Festival.c
+++ Color Festival.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// function to check if value exists in linked list
+int exists(struct Node* head, int val) {
+    struct Node* temp = head;
+    
+    while (temp != NULL) {
+        if (temp->data == val)
+            return 1;
+        temp = temp->next;
+    }
+    
+    return 0;
+}
+
+// function to insert node at beginning
+struct Node* insert(struct Node* head, int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    
+    newNode->data = val;
+    newNode->next = head;
+    
+    return newNode;
+}
 
 int main() {
     int T;
@@ -8,13 +37,14 @@ int main() {
         int N;
         scanf("%d", &N);
 
-        int color[101] = {0};
+        struct Node* head = NULL;
         int c, count = 0;
 
         for (int i = 0; i < N; i++) {
             scanf("%d", &c);
-            if (color[c] == 0) {
-                color[c] = 1;
+
+            if (!exists(head, c)) {
+                head = insert(head, c);
                 count++;
             }
         }
