ca290acbb13c8b84f183bdc802c4c83550754223
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-20 22:07:56 +0530

    Create PerfectEighthPower.c

---
+++ PerfectEighthPower.c
@@ -0,0 +1,24 @@
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int t = 0; t < T; t++) {
+        int N;
+        scanf("%d", &N);
+
+        int count2 = 0, x;
+
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &x);
+            if (x == 2) count2++;
+        }
+
+        if (count2 % 8 == 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+
+    return 0;
+}
