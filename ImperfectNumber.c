f0fe63f7ca1ba33d7144a1161ebc0b0cdd4428db
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-25 21:58:45 +0530

    Create ImperfectNumber.c

---
+++ ImperfectNumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        if ((n % 2 == 0 || n % 5 == 0) && !(n % 2 == 0 && n % 5 == 0)) {
+            printf("0\n");
+        }
+        else if (n % 10 == 0) {
+            printf("2\n");
+        }
+        else {
+            printf("1\n");
+        }
+    }
+
+    return 0;
+}
