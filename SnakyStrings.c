163bbc72aebf0b72d7f7f35a60752a41c357b0d9
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-25 20:56:39 +0530

    Create SnakyStrings.c

---
+++ SnakyStrings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char s[5];
+    scanf("%s",&s);
+    if(s[0]=='s' || s[3]=='s'){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+    
+
+}
+
